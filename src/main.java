a0aeaf7b91cf06dd454e05ecef9648e086683362
TheLi4e <li4e99@gmail.com>
2022-12-05 17:03:54 +0200

    Add lesson3

--- src/main.java
+++ src/main.java
@@ -2,28 +2,10 @@ package src;
 
 public class main {
     public static void main(String[] args) {
-        Lesson2.checkIfUserNumberIsFloat();
-        Lesson2.divideArrayNums();
-        try {
-            int a = 90;
-            int b = 0;
-            String x = null;
-            int[] abc = {1, 2};
-            System.out.println(abc[3]); // Вызовет IndexOutOfBoundsException
-            abc[3] = 9; // Вызовет IndexOutOfBoundsException
-            System.out.println(x.length()); // Вызовет NullPointerException
-            System.out.println(a / b); // Вызовет ArithmeticException
-            Lesson2.printSum(23, 234); // Исключение невозможно
-
-        } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!");
-        } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Массив выходит за пределы своего размера!");
-        } catch (ArithmeticException e) {
-            System.out.println("Деление на ноль!");
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        }
-        Lesson2.printUserString();
+        // 12.12.12 Иванов 999999999 Иванович Иван м
+        // 12.12.12 Иванов 92873645аа Иванович Петр м
+        // Анна ж 12.12.12 Петровна Сидорова 7632648712
+        // ж 0090878769764 Петрова 22.09.45 Сергеевна Лидия
+        Lesson3.writeInUserFile();
     }
 }
